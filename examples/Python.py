bdea8a8b669643ba1c2e60be2714a6b3705624f0
Kuzeko <web@kuzeko.com>
2018-10-08 13:24:15 +0200

    fixed python

--- examples/Python.py
+++ examples/Python.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     print "Hello, %s" % instance.name
     # Comments
     for i in range( 1, 100):
-        a = [ x+1 for x in range(1, i) ]
-
-#	print "Mixing tabs and spaces is bad"
+      a = [ x+1 for x in range(1, i) ]
+    
+#	 print "Mixing tabs and spaces is bad"
